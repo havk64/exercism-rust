1fd6688c04bb8f0c0a45fe95387d27e5cfad9396
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-16 21:45:45 +0100

    refactor: replace if/else with return of expression that returns a boolean

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,11 +1,7 @@
 pub fn is_leap_year(year: u64) -> bool {
     if year % 4 == 0 {
         if year % 100 == 0 {
-            if year % 400 == 0 {
-                true
-            } else {
-                false
-            };
+            return year % 400 == 0
         }
         true
     } else {
