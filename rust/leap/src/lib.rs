2d00321ab3bf8548012fc3b25ad8ae3c80680d0e
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-16 21:14:04 +0100

    style: add missing semicolon to return statements

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -2,13 +2,13 @@ pub fn is_leap_year(year: u64) -> bool {
     if year % 4 == 0 {
         if year % 100 == 0 {
             if year % 400 == 0 {
-                return true
+                return true;
             } else {
-                return false
+                return false;
             }
         }
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 }
