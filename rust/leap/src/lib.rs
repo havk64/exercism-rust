28668a808669f40987d4bf323eee71162db6091e
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-16 21:44:44 +0100

    refactor: remove needless returns

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -2,13 +2,13 @@ pub fn is_leap_year(year: u64) -> bool {
     if year % 4 == 0 {
         if year % 100 == 0 {
             if year % 400 == 0 {
-                return true;
+                true
             } else {
-                return false;
-            }
+                false
+            };
         }
-        return true;
+        true
     } else {
-        return false;
+        false
     }
 }
