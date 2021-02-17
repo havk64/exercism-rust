dd2f055f8d6f5c8b6d9fc3534391c565d3945d25
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-17 21:31:35 +0100

    refactor: change variable name to prevent conflict with rust internals

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,17 +1,17 @@
 pub fn raindrops(n: u32) -> String {
-    let mut str = String::new();
+    let mut s = String::new();
 
     if n % 3 == 0 {
-        str.push_str("Pling");
+        s.push_str("Pling");
     }
     if n % 5 == 0 {
-        str.push_str("Plang");
+        s.push_str("Plang");
     }
     if n % 7 == 0 {
-        str.push_str("Plong");
+        s.push_str("Plong");
     }
-    if str == "".to_string() {
-        str.push_str(&n.to_string());
+    if s.is_empty() {
+        s.push_str(&n.to_string());
     }
-    str
+    s
 }
