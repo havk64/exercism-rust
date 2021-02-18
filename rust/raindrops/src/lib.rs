5931c80b8553840e4eeca52e6dc66da24d2ecccc
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-18 14:28:26 +0100

    doc(raindrops): add minimim explanation about the solution

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,14 +1,17 @@
+// Constant with factors and respective 'sounds'
 const OPTIONS: [(u32, &'static str); 3] = [(3, "Pling"), (5, "Plang"), (7, "Plong")];
 
 pub fn raindrops(n: u32) -> String {
     let mut s = String::new();
 
+    // Iterate over constant and processing each item accordingly
     for item in OPTIONS.iter() {
         if n % item.0 == 0 {
             s.push_str(item.1)
         }
     }
 
+    // If after the processing the string is still empty parse number as string
     if s.is_empty() {
         s = n.to_string()
     }
