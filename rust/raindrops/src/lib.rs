3facd19e3b9fe87fe6077df62cd627a71f432cee
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-18 12:48:57 +0100

    refactor(raindrops): apply rustfmt on raindrops solution

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,14 +1,12 @@
-const OPTIONS: [(u32, &'static str); 3] = [
-    (3, "Pling"),
-    (5, "Plang"),
-    (7, "Plong")
-];
+const OPTIONS: [(u32, &'static str); 3] = [(3, "Pling"), (5, "Plang"), (7, "Plong")];
 
 pub fn raindrops(n: u32) -> String {
     let mut s = String::new();
 
     for item in OPTIONS.iter() {
-        if n % item.0 == 0 { s.push_str(item.1) }
+        if n % item.0 == 0 {
+            s.push_str(item.1)
+        }
     }
 
     if s.is_empty() {
