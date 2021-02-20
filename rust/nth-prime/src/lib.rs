101de52d82b29f64ed6eb2f02bbe6f576efc721a
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-20 18:42:19 +0100

    feat: add first solution to nth-prime exercise

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes: Vec<u32> = Vec::new();
+    let mut is_prime = true;
+
+    for x in 2..u32::MAX  {
+        for y in primes[..].iter() {
+            if x % y == 0 {
+                is_prime = false;
+                break;
+            }
+        }
+        if is_prime {
+            primes.push(x);
+        }
+        if primes.len() == (n + 1) as usize {
+            return x;
+        }
+        is_prime = true;
+    }
+    0
 }
