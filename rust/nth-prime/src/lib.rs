d0ec1a2b0f58bd195fe9d4aa00f108f6dcadcd9e
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-20 22:15:00 +0100

    style: format solution with rustfmt

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,9 +1,12 @@
-fn  is_prime(n: u32) -> bool {
+fn is_prime(n: u32) -> bool {
     !(2..n - 1).any(|i| n % i == 0)
 }
 pub fn nth(n: u32) -> u32 {
     match n {
         0 => 0,
-        n => (1..).filter(|c| -> bool {is_prime(*c)}).nth((n + 1) as usize).unwrap(),
+        n => (1..)
+            .filter(|c| -> bool { is_prime(*c) })
+            .nth((n + 1) as usize)
+            .unwrap(),
     }
 }
