253d95df172270f5a450c0ae6a9e2decffa729e1
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-21 19:50:58 +0100

    style(nth-prime): apply rustfmt suggestions

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,18 +1,16 @@
 fn is_prime(primes: &mut Vec<u32>, c: &u32) -> bool {
-            if !primes.iter().any(|i| c % i == 0) {
-                primes.push(*c);
-                true
-            } else {
-                false
-            }
+    if !primes.iter().any(|i| c % i == 0) {
+        primes.push(*c);
+        true
+    } else {
+        false
+    }
 }
 pub fn nth(n: u32) -> u32 {
     let mut primes: Vec<u32> = Vec::new();
 
     (2..)
-        .filter(|c| -> bool {
-            is_prime(&mut primes, c)
-        })
-        .nth(n  as usize)
+        .filter(|c| -> bool { is_prime(&mut primes, c) })
+        .nth(n as usize)
         .unwrap()
 }
