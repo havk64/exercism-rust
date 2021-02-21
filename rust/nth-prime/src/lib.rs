fdf277475db942fbde0180ca8e8ec2d8e7ff2152
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-21 19:50:14 +0100

    refactor(nth-prime): move logic of filter to separate function

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,14 +1,17 @@
-pub fn nth(n: u32) -> u32 {
-    let mut primes: Vec<u32> = Vec::new();
-
-    (2..)
-        .filter(|c| -> bool {
+fn is_prime(primes: &mut Vec<u32>, c: &u32) -> bool {
             if !primes.iter().any(|i| c % i == 0) {
                 primes.push(*c);
                 true
             } else {
                 false
             }
+}
+pub fn nth(n: u32) -> u32 {
+    let mut primes: Vec<u32> = Vec::new();
+
+    (2..)
+        .filter(|c| -> bool {
+            is_prime(&mut primes, c)
         })
         .nth(n  as usize)
         .unwrap()
