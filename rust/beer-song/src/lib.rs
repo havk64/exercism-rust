5344cc13126aedaa1dfc7ea848056c12d8509fbd
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-23 22:01:58 +0100

    refactor(beer-song): merge match arm to reduce repetition

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -2,7 +2,7 @@ fn beer_count(n: u32) -> String {
     let middle = "of beer";
 
     match n {
-        0 => format!("No more bottles {}", middle),
+        0 => format!("No more bottles {} ", middle),
         1 => format!("1 bottle {}", middle),
         n => format!("{} bottles {}", n, middle),
     }
@@ -21,22 +21,14 @@ pub fn verse(n: u32) -> String {
             answer.push_str(&format!("{} {}.\n", beer_count(99), tail));
             answer
         }
-        1 => {
-            let mut answer = format!(
-                "{} {}, {}.\nTake it down and pass it around, ",
-                beer_count(1),
-                tail,
-                beer_count(1).to_lowercase()
-            );
-            answer.push_str(&format!("{} {}.\n", beer_count(0).to_lowercase(), tail));
-            answer
-        }
         n => {
+            let unit = if n == 1 { "it" } else { "one" };
             let mut answer = format!(
-                "{} {}, {}.\nTake one down and pass it around, ",
+                "{} {}, {}.\nTake {} down and pass it around, ",
                 beer_count(n),
                 tail,
-                beer_count(n).to_lowercase()
+                beer_count(n).to_lowercase(),
+                unit,
             );
             answer.push_str(&format!("{} {}.\n", beer_count(n - 1).to_lowercase(), tail));
             answer
