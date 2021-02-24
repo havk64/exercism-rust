4476df8bd9aa29a8f4bc7dae813d85df16bbde65
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-24 22:34:46 +0100

    feat(proverb): add first (dirty) working solution

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut list = list.iter();
+    let first: &str = list.next().unwrap();
+    let mut shadow = first;
+    let mut s: Vec<String> = Vec::new();
+    for item in list {
+        s.push(format!("For want of a {} the {} was lost.", shadow, item));
+        shadow = item;
+    }
+    s.push(format!("And all for the want of a {}.", first));
+    s.join("\n")
 }
