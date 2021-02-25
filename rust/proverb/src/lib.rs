8b136116be2e50353005f365812e9f711e37e545
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-25 22:15:15 +0100

    refactor(proverb): implement solution to proverb using iterators

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,17 +1,11 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut list = list.iter();
-    let first: &str = if let Some(first) = list.next() {
-        first
-    } else {
+    if list.is_empty() {
         return "".to_string();
-    };
-    let mut swap = first;
-    let mut s: Vec<String> = Vec::new();
-
-    for item in list {
-        s.push(format!("For want of a {} the {} was lost.", swap, item));
-        swap = item;
     }
-    s.push(format!("And all for the want of a {}.", first));
-    s.join("\n")
+
+    list.windows(2)
+        .map(|slice|format!("For want of a {} the {} was lost.", slice[0], slice[1]))
+        .chain(std::iter::once(format!("And all for the want of a {}.", list[0])))
+        .collect::<Vec<String>>()
+        .join("\n")
 }
