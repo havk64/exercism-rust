38e0d69c3f3e44ef62e409bfd4776c7382cb5194
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-25 21:51:25 +0100

    refactor(proverb): more meaningful variable names

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -5,12 +5,12 @@ pub fn build_proverb(list: &[&str]) -> String {
     } else {
         return "".to_string();
     };
-    let mut shadow = first;
+    let mut swap = first;
     let mut s: Vec<String> = Vec::new();
 
     for item in list {
-        s.push(format!("For want of a {} the {} was lost.", shadow, item));
-        shadow = item;
+        s.push(format!("For want of a {} the {} was lost.", swap, item));
+        swap = item;
     }
     s.push(format!("And all for the want of a {}.", first));
     s.join("\n")
