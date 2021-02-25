2ab8ace46e3c773175eab79297151a762a2d1157
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-25 22:36:39 +0100

    refactor(proverb): simply solution to proverb exercise by replacing join with hardcoded char

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,11 +1,10 @@
 pub fn build_proverb(list: &[&str]) -> String {
     list.windows(2)
-        .map(|slice| format!("For want of a {} the {} was lost.", slice[0], slice[1]))
+        .map(|slice| format!("For want of a {} the {} was lost.\n", slice[0], slice[1]))
         .chain(
             list.iter()
                 .take(1)
                 .map(|first| format!("And all for the want of a {}.", first)),
         )
-        .collect::<Vec<String>>()
-        .join("\n")
+        .collect()
 }
