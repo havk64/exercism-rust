c650e231b154fd139c8481b0d8839491c8144d8b
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-25 22:26:34 +0100

    refactor(proverb): replace check for empty list with take method

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,11 +1,10 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    if list.is_empty() {
-        return "".to_string();
-    }
-
     list.windows(2)
         .map(|slice|format!("For want of a {} the {} was lost.", slice[0], slice[1]))
-        .chain(std::iter::once(format!("And all for the want of a {}.", list[0])))
+        .chain(
+            list.iter()
+            .take(1)
+            .map(|first|format!("And all for the want of a {}.", first)))
         .collect::<Vec<String>>()
         .join("\n")
 }
