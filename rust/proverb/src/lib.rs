9a66ca0abf3111d9729e34fd014fed6418d47286
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-25 21:48:23 +0100

    refactor(proverb): improve solution to proverb exercise to handle last test

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,8 +1,13 @@
 pub fn build_proverb(list: &[&str]) -> String {
     let mut list = list.iter();
-    let first: &str = list.next().unwrap();
+    let first: &str = if let Some(first) = list.next() {
+        first
+    } else {
+        return "".to_string();
+    };
     let mut shadow = first;
     let mut s: Vec<String> = Vec::new();
+
     for item in list {
         s.push(format!("For want of a {} the {} was lost.", shadow, item));
         shadow = item;
