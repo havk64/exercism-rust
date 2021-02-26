3335162c93ab48c1dfdb1339caf92202464ed68c
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-26 21:09:26 +0100

    refactor(difference-of-squares): add comments before submiting the final solution

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,13 +1,15 @@
+// We need to make the trait available to use its method, .sub() in this case
 use std::ops::Sub;
 
 pub fn square_of_sum(n: u32) -> u32 {
-    (1..(n+1)).sum::<u32>().pow(2)
+    (1..n+1).sum::<u32>().pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..(n+1)).map(|x| x.pow(2)).sum::<u32>()
+    (1..n+1).map(|x| x.pow(2)).sum::<u32>()
 }
 
 pub fn difference(n: u32) -> u32 {
+    // Using the 'sub' method from the std::ops::Sub trait:
     square_of_sum(n).sub(sum_of_squares(n))
 }
