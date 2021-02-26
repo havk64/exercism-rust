25581f704621628c304e84ea856775da74f0e97d
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-26 20:55:24 +0100

    feat(difference-of-squares): add first working solution to difference-of-squares exercise

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,14 +1,13 @@
+use std::ops::Sub;
+
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+    (1..(n+1)).sum::<u32>().pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+    (1..(n+1)).map(|x| x.pow(2)).sum::<u32>()
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    square_of_sum(n).sub(sum_of_squares(n))
 }
