2a45e34af19b444c2855d2e4d39236105e3d108e
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-27 22:11:39 +0100

    refactor(sum-of-multiples): add condition to exclude zero from factor's slice

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -2,6 +2,6 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     (1..limit)
         .filter(|n|
             factors.iter()
-                .any(|f| n % f == 0))
+                .any(|&f| f > 0 && n % f == 0))
         .sum()
 }
