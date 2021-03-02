f32c2dce8b8e0dbeb24930549c245e48bc225af0
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-02 21:55:06 +0100

    refactor(grains): remove unnecessary type, replace iterator with bitwise operator

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,10 +1,10 @@
 pub fn square(s: u32) -> u64 {
     match s {
-        1..=64 => 1u64 << (s - 1),
+        1..=64 => 1 << (s - 1),
         _ => panic!("Square must be between 1 and 64"),
     }
 }
 
 pub fn total() -> u64 {
-    (1..65).map(square).sum()
+    !0x0
 }
