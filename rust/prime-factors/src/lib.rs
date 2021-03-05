f68401a748a1e3f824212b766c564a611fcbbe25
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-05 22:50:32 +0100

    feat(prime-factors): add first working solution to prime-factors exercise

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,26 @@
+fn is_prime(c: &u64, primes: &mut Vec<u64>) -> bool {
+    if !primes.iter().any(|i|c % i == 0) {
+        primes.push(*c);
+        false
+    } else {
+        true
+    }
+}
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut primes = Vec::new();
+    let mut result = Vec::new();
+    let mut x = n;
+
+    let _ = (2..x).filter(|c| is_prime(c, &mut primes))
+        .collect::<Vec<_>>();
+    while x > 0 {
+        for p in primes.iter() {
+            if x % p == 0 {
+                x /= p;
+                result.push(*p);
+                break;
+            }
+        }
+    };
+    result
 }
