453e1c90f874b34e2d5e068a8148099a83acf008
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-06 21:22:00 +0100

    refactor(prime-factors): remove logic to create vector of prime numbers

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,24 +1,12 @@
-fn is_prime(c: &u64, primes: &mut Vec<u64>) -> bool {
-    if !primes.iter().any(|i| c % i == 0) {
-        primes.push(*c);
-        false
-    } else {
-        true
-    }
-}
 pub fn factors(n: u64) -> Vec<u64> {
-    let mut primes = Vec::new();
     let mut result = Vec::new();
-    let mut x = n;
+    let mut x  = n;
 
-    let _ = (2..x)
-        .filter(|c| is_prime(c, &mut primes))
-        .collect::<Vec<_>>();
     while x > 1 {
-        for p in primes.iter() {
+        for p in 2..n {
             if x % p == 0 {
                 x /= p;
-                result.push(*p);
+                result.push(p);
                 break;
             }
         }
