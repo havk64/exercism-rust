688ddd9f270499548a3c222f86c871289140b72c
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-07 19:27:14 +0100

    refactor(prime-factors): replace unwrap with 'if let'

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn factors(mut n: u64) -> Vec<u64> {
     let mut result = Vec::new();
-    let mut c  = 2..;
+    let mut range  = 2..;
 
     while n > 1 {
-        let c = c.next().unwrap();
-
-        while n % c == 0 {
-            n /= c;
-            result.push(c);
+        if let Some(c) = range.next() {
+            while n % c == 0 {
+                n /= c;
+                result.push(c);
+            }
         }
     }
     result
