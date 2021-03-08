bf8987940263e3928704680027004076dbc62cb4
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-08 21:18:09 +0100

    refactor(armstrong-numbers): improve decompose function by removing one loop

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,22 +1,14 @@
-fn decompose(mut num: u32) -> Vec<u32> {
+fn decompose(mut num: u32, base: u32) -> Vec<u32> {
     let mut digits: Vec<u32> = Vec::new();
-    let mut power = 1;
-    let mut n = num;
 
-    while n > 9 {
-        n /= 10;
-        power *= 10;
-    }
-
-    while power > 0 {
-        digits.push(num / power);
-        num %= power;
-        power /= 10;
+    while num > 0 {
+        digits.push(num % base);
+        num /= 10;
     }
     digits
 }
 pub fn is_armstrong_number(num: u32) -> bool {
-    let dec = decompose(num);
+    let dec = decompose(num, 10);
     let power = dec.len() as u32;
 
     dec.iter()
