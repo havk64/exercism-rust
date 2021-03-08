a431c9cf97e8955ee2e6ea1dd2682ce49244b298
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-08 21:30:50 +0100

    refactor(armostrong-numbers): change order of comparison to improve readability

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -11,7 +11,7 @@ pub fn is_armstrong_number(num: u32) -> bool {
     let dec = decompose(num, 10);
     let power = dec.len() as u32;
 
-    dec.iter()
+    num == dec.iter()
         .map(|&digit| digit.pow(power))
-        .sum::<u32>() == num
+        .sum()
 }
