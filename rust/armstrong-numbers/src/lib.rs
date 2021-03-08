0c78a240b1958f4425bffe90b1af1fb244ee2a00
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-08 20:58:01 +0100

    feat(armstrong-numbers): add first working solution for armstrong-numbers exercise

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,25 @@
+fn decompose(mut num: u32) -> Vec<u32> {
+    let mut digits: Vec<u32> = Vec::new();
+    let mut power = 1;
+    let mut n = num;
+
+    while n > 9 {
+        n /= 10;
+        power *= 10;
+    }
+
+    while power > 0 {
+        digits.push(num / power);
+        num %= power;
+        power /= 10;
+    }
+    digits
+}
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let dec = decompose(num);
+    let power = dec.len() as u32;
+
+    dec.iter()
+        .map(|&digit| digit.pow(power))
+        .sum::<u32>() == num
 }
