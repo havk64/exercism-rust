00cc4f9e4ffebecee81a5d192b50ee3772800a85
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-11 21:33:36 +0100

    feat(gigasecond): add first working solution to gigasecond exercise

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, Duration};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    start + Duration::seconds(10_i64.pow(9))
 }
