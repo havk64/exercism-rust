168e4063179f027da7541be490bf39e8e9c144b0
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-11 21:58:18 +0100

    refactor(gigasecond): add alternative solution to gigasecond using checked function

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,12 @@
-use chrono::{DateTime, Utc, Duration};
+use {
+    chrono::{
+        DateTime,
+        Utc,
+        Duration,
+    },
+};
 
-// Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(10_i64.pow(9))
+    start.checked_add_signed(Duration::seconds(10i64.pow(9)))
+        .expect("Overflow")
 }
