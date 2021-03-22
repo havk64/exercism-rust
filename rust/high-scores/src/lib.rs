5144437f4ffc105c6543df9de5984ed7598c2c2e
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-22 23:21:05 +0100

    feat(high-scores): add first (incomplete) solution to high-scores

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,24 +1,31 @@
 #[derive(Debug)]
-pub struct HighScores;
+pub struct HighScores<'a> {
+    scores: &'a [u32]
+}
 
-impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        unimplemented!(
-            "Construct a HighScores struct, given the scores: {:?}",
-            scores
-        )
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
     }
 
     pub fn scores(&self) -> &[u32] {
-        unimplemented!("Return all the scores as a slice")
+        self.scores
     }
 
     pub fn latest(&self) -> Option<u32> {
-        unimplemented!("Return the latest (last) score")
+        if let Some(&x) = self.scores.last() {
+            Some(x)
+        } else {
+            None
+        }
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        unimplemented!("Return the highest score")
+        if let Some(&x) = self.scores.iter().max() {
+            Some(x)
+        } else {
+            None
+        }
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
