63313b8e4f25456d8fa173e70ccf4d0e11604f8e
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-03-30 23:18:30 +0200

    refactor: add another sketch of possible solution

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,14 +1,21 @@
 use regex::RegexSet;
 
 pub fn reply(message: &str) -> &str {
-    let re = RegexSet::new(&[
+    let set = RegexSet::new(&[
         r"?",
         r"<upper>",
         r"<yell question",
         r"<empty>",
     ]).unwrap();
+    let matches = set.matches(message);
 
-    match message {
-        _ => "Whatever.",
+    if matches.matched(1) {
+        "Whatever."
+    } else if matches.matched(2) {
+        "Something else"
+    } else if matches.matched(3) {
+        "Yell"
+    } else {
+        "Else"
     }
 }
