38f5eca3df25d95177618a6b3a72677cc02ca241
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-04-04 23:32:09 +0200

    refactor(bob): add if/else to reduce code boilerplate, bob exercise

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,7 +1,12 @@
 pub fn reply(message: &str) -> &str {
     match message.trim() {
-        msg if msg.to_uppercase() == msg && msg.contains(char::is_alphabetic) && msg.ends_with("?") => "Calm down, I know what I'm doing!",
-        msg if msg.to_uppercase() == msg && msg.contains(char::is_alphabetic) => "Whoa, chill out!",
+        msg if msg.to_uppercase() == msg && msg.contains(char::is_alphabetic) => {
+            if msg.ends_with("?") {
+                "Calm down, I know what I'm doing!"
+            } else {
+                "Whoa, chill out!"
+            }
+        },
         msg if msg.ends_with("?") => "Sure.",
         msg if msg.is_empty() => "Fine. Be that way!",
         _ => "Whatever."
