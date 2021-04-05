f36750beafa30947403b1d8407e215929d69efb9
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-04-05 22:39:53 +0200

    feat(matching-brackets): add first working solution to matching-brackets exercise

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,35 @@
+enum Bracket {
+    Open(char),
+    Close(char),
+}
+
+impl Bracket {
+    fn from_char(c: char) -> Option<Bracket> {
+        match c {
+            '{' | '[' | '(' => Some(Bracket::Open(c)),
+            '}' => Some(Bracket::Close('{')),
+            ']' => Some(Bracket::Close('[')),
+            ')' => Some(Bracket::Close('(')),
+            _ => None,
+        }
+    }
+}
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut brackets = vec![];
+
+    for c in string.chars() {
+        match Bracket::from_char(c) {
+            Some(Bracket::Open(open_char)) => {
+                brackets.push(open);
+            }
+            Some(Bracket::Close(close_char)) => {
+                if brackets.pop() != Some(close_char) {
+                    return false;
+                }
+            }
+            _ => {}
+        }
+    }
+    brackets.is_empty()
 }
