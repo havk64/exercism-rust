680636ec9ec984c96e7cdc2f84b83b6fe2e1b7f2
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-04-06 21:39:25 +0200

    refactor(matching-brackets): improve/simplify solution for matching-brackets

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,35 +1,14 @@
-enum Bracket {
-    Open(char),
-    Close(char),
-}
-
-impl Bracket {
-    fn from_char(c: char) -> Option<Bracket> {
-        match c {
-            '{' | '[' | '(' => Some(Bracket::Open(c)),
-            '}' => Some(Bracket::Close('{')),
-            ']' => Some(Bracket::Close('[')),
-            ')' => Some(Bracket::Close('(')),
-            _ => None,
-        }
-    }
-}
-
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let mut brackets = vec![];
+    let mut stack = vec![];
 
     for c in string.chars() {
-        match Bracket::from_char(c) {
-            Some(Bracket::Open(open_char)) => {
-                brackets.push(open);
-            }
-            Some(Bracket::Close(close_char)) => {
-                if brackets.pop() != Some(close_char) {
-                    return false;
-                }
-            }
-            _ => {}
+        match c {
+            '{' | '[' | '(' => stack.push(c),
+            ')' => if stack.pop() != Some('(') { return false },
+            ']' => if stack.pop() != Some('[') { return false },
+            '}' => if stack.pop() != Some('{') { return false },
+            _ => (),
         }
     }
-    brackets.is_empty()
+    stack.is_empty()
 }
