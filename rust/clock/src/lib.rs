29f70ba50bcc7d04a76fbcc70ad6989cf4534801
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-04-16 23:41:34 +0200

    feat(clock): add first possible solution for Clock exercise

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,27 @@
-pub struct Clock;
+use std::cell::Cell;
+use std::fmt::{Display,Formatter,Result};
+
+#[derive(Debug)]
+pub struct Clock {
+    hours: i32,
+    minutes: Cell<i32>,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock { hours, minutes: Cell::new(minutes) }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock {
+            hours: self.hours,
+            minutes: Cell::new(self.minutes.get() + minutes),
+        }
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}:{}", self.hours, self.minutes.get())
     }
 }
